f3fcb485c23ba2af3aa96bad385fd08c46cc4426
Nathanael Wong <natgeo.wong@outlook.com>
2021-08-23 13:17:56 -0400

    Updating CI and Docs information

--- docs/make.jl
+++ docs/make.jl
@@ -4,20 +4,21 @@ using Documenter
 DocMeta.setdocmeta!(ERA5Reanalysis, :DocTestSetup, :(using ERA5Reanalysis); recursive=true)
 
 makedocs(;
-    modules=[ERA5Reanalysis],
-    authors="Nathanael Wong <natgeo.wong@outlook.com>",
-    repo="https://github.com/natgeo-wong/ERA5Reanalysis.jl/blob/{commit}{path}#{line}",
-    sitename="ERA5Reanalysis.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+    modules  = [ERA5Reanalysis],
+    doctest  = false,
+    format   = Documenter.HTML(;
+        prettyurls=get(ENV,"CI","false") == "true",
         canonical="https://natgeo-wong.github.io/ERA5Reanalysis.jl",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
+    authors  = "Nathanael Wong <natgeo.wong@outlook.com>",
+    sitename = "ERA5Reanalysis.jl",
+    pages    = [
+        "Home"                        => "index.md",
     ],
 )
 
-deploydocs(;
-    repo="github.com/natgeo-wong/ERA5Reanalysis.jl",
+deploydocs(
+    repo = "github.com/natgeo-wong/ERA5Reanalysis.jl.git",
+    devbranch = "main"
 )
