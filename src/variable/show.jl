4c4e715fb3c7684c5f5311a4c1947caad6349427
Nathanael Wong <natgeo.wong@outlook.com>
2021-08-24 01:09:48 -0400

    show() functions for variable structures

---
+++ src/variable/show.jl
@@ -0,0 +1,22 @@
+function show(io::IO, evar::SingleLevel)
+    print(
+		io,
+		"The Single-Level Variable $(evar.regID) has the following properties:\n",
+		"    Variable ID    (varID) : ", evar.varID, '\n',
+		"    Long Name      (lname) : ", evar.lname, '\n',
+		"    Variable Name  (vname) : ", evar.vname, '\n',
+		"    Variable Units (units) : ", evar.units, '\n',
+	)
+end
+
+function show(io::IO, evar::PressureLevel)
+    print(
+		io,
+		"The Pressure-Level Variable $(evar.regID) has the following properties:\n",
+		"    Variable ID    (varID) : ", evar.varID, '\n',
+		"    Long Name      (lname) : ", evar.lname, '\n',
+		"    Variable Name  (vname) : ", evar.vname, '\n',
+		"    Variable Units (units) : ", evar.units, '\n',
+		"    Pressure Level (plvl)  : ", evar.plvl,  '\n',
+	)
+end
