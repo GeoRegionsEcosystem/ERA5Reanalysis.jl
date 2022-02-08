b89962d9dd1d7e7bab95d57921f076d08cbc77d0
Nathanael Wong <natgeo.wong@outlook.com>
2022-02-07 19:07:00 -0500

    Adding save capabilities

---
+++ src/save.jl
@@ -0,0 +1,61 @@
+function save(
+    data :: Array{<:Real,3},
+    dt   :: Date,
+    e5ds :: ERA5Dataset,
+    evar :: ERA5Variable,
+    ereg :: ERA5Region
+)
+
+    @info "$(modulelog()) - Saving raw $(e5ds.lname) $(evar.vname) data in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) ..."
+
+    fnc = e5dfnc(e5ds,evar,egeo,dt)
+    if isfile(fnc)
+        @info "$(modulelog()) - Stale NetCDF file $(fnc) detected.  Overwriting ..."
+        rm(fnc);
+    end
+    ds = NCDataset(fnc,"c",attrib = Dict(
+        "Conventions" => "CF-1.6",
+        "history"     => "Created on $(Dates.now())"
+    ));
+
+    nhr = ehr * daysinmonth(date);
+    scale,offset = erancoffsetscale(data);
+
+    ds.dim["longitude"] = ereg["size"][1];
+    ds.dim["latitude"]  = ereg["size"][2];
+    ds.dim["time"] = nhr
+
+    nclongitude = defVar(ds,"longitude",Float32,("longitude",),attrib = Dict(
+        "units"     => "degrees_east",
+        "long_name" => "longitude",
+    ))
+
+    nclatitude = defVar(ds,"latitude",Float32,("latitude",),attrib = Dict(
+        "units"     => "degrees_north",
+        "long_name" => "latitude",
+    ))
+
+    nctime = defVar(ds,"time",Int32,("time",),attrib = Dict(
+        "units"     => "hours since $(dt) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian",
+    ))
+
+    ncvar = defVar(ds,epar["ID"],Int16,("longitude","latitude","time"),attrib = Dict(
+        "long_name"     => evar.lname,
+        "full_name"     => evar.vname,
+        "units"         => evar.units,
+        "scale_factor"  => scale,
+        "add_offset"    => offset,
+        "_FillValue"    => Int16(-32767),
+        "missing_value" => Int16(-32767),
+    ))
+
+    nclongitude[:] = ereg["lon"]; nclatitude[:] = ereg["lat"]
+    nctime[:] = (collect(1:nhr).-1) * ehr; ncvar[:] = data;
+
+    close(ds)
+
+    @info "$(modulelog()) - Raw $(uppercase(e5ds.lname)) $(evar.vname) in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) has been saved into $(fnc)."
+
+end
