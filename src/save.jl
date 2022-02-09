4372a2a237692be598599ed33ebf9ebda463e00d
Nathanael Wong <natgeo.wong@outlook.com>
2022-02-08 22:06:55 -0500

    Adding method for save

--- src/save.jl
+++ src/save.jl
@@ -3,6 +3,79 @@ function save(
     dt   :: Date,
     e5ds :: ERA5Dataset,
     evar :: ERA5Variable,
+    ereg :: ERA5Region,
+    lsd  :: LandSea
+)
+
+    @info "$(modulelog()) - Saving raw $(e5ds.lname) $(evar.vname) data in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) ..."
+
+    fnc = e5dfnc(e5ds,evar,ereg,dt)
+    fol = dirname(fnc); if !isdir(fol); mkpath(fol) end
+    if isfile(fnc)
+        @info "$(modulelog()) - Stale NetCDF file $(fnc) detected.  Overwriting ..."
+        rm(fnc);
+    end
+    ds = NCDataset(fnc,"c",attrib = Dict(
+        "Conventions" => "CF-1.6",
+        "history"     => "Created on $(Dates.now()) with ERA5Reanalysis.jl",
+        "comments"    => "ERA5Reanalysis.jl creates NetCDF files in the same format that data is saved on the Climate Data Store"
+    ))
+
+    if typeof(evar) <: SingleVariable
+        ds.attrib["doi"] = e5ds.sldoi
+    elseif typeof(evar) <: PressureVariable
+        ds.attrib["doi"] = e5ds.pldoi
+    end
+
+    nhr = 24 * daysinmonth(dt);
+    scale,offset = ncoffsetscale(data);
+
+    ds.dim["longitude"] = length(lsd.lon);
+    ds.dim["latitude"]  = length(lsd.lat);
+    ds.dim["time"] = nhr
+
+    nclon = defVar(ds,"longitude",Float32,("longitude",),attrib = Dict(
+        "units"     => "degrees_east",
+        "long_name" => "longitude",
+    ))
+
+    nclat = defVar(ds,"latitude",Float32,("latitude",),attrib = Dict(
+        "units"     => "degrees_north",
+        "long_name" => "latitude",
+    ))
+
+    nctime = defVar(ds,"time",Int32,("time",),attrib = Dict(
+        "units"     => "hours since $(dt) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian",
+    ))
+
+    ncvar = defVar(ds,epar["ID"],Int16,("longitude","latitude","time"),attrib = Dict(
+        "long_name"     => evar.lname,
+        "full_name"     => evar.vname,
+        "units"         => evar.units,
+        "scale_factor"  => scale,
+        "add_offset"    => offset,
+        "_FillValue"    => Int16(-32767),
+        "missing_value" => Int16(-32767),
+    ))
+
+    nclon[:]  = lsd.lon
+    nclat[:]  = lsd.lat
+    nctime[:] = (collect(1:nhr).-1) * 24
+    ncvar[:]  = data;
+
+    close(ds)
+
+    @info "$(modulelog()) - Raw $(uppercase(e5ds.lname)) $(evar.vname) in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) has been saved into $(fnc)."
+
+end
+
+function save(
+    data :: AbstractArray{<:Real,3},
+    dt   :: Date,
+    e5ds :: ERA5Dataset,
+    evar :: ERA5Variable,
     ereg :: ERA5Region
 )
 
@@ -29,16 +102,17 @@ function save(
     nhr = 24 * daysinmonth(dt);
     scale,offset = ncoffsetscale(data);
 
-    ds.dim["longitude"] = ereg["size"][1];
-    ds.dim["latitude"]  = ereg["size"][2];
+    lsd = getLandSea(e5ds,ereg)
+    ds.dim["longitude"] = length(lsd.lon);
+    ds.dim["latitude"]  = length(lsd.lat);
     ds.dim["time"] = nhr
 
-    nclongitude = defVar(ds,"longitude",Float32,("longitude",),attrib = Dict(
+    nclon = defVar(ds,"longitude",Float32,("longitude",),attrib = Dict(
         "units"     => "degrees_east",
         "long_name" => "longitude",
     ))
 
-    nclatitude = defVar(ds,"latitude",Float32,("latitude",),attrib = Dict(
+    nclat = defVar(ds,"latitude",Float32,("latitude",),attrib = Dict(
         "units"     => "degrees_north",
         "long_name" => "latitude",
     ))
@@ -59,8 +133,10 @@ function save(
         "missing_value" => Int16(-32767),
     ))
 
-    nclongitude[:] = ereg["lon"]; nclatitude[:] = ereg["lat"]
-    nctime[:] = (collect(1:nhr).-1) * 24; ncvar[:] = data;
+    nclon[:]  = lsd.lon
+    nclat[:]  = lsd.lat
+    nctime[:] = (collect(1:nhr).-1) * 24
+    ncvar[:]  = data;
 
     close(ds)
 
