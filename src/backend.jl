47f00d359a9d4482c1974a628a664a07ab0f25bc
Nathanael Wong <natgeo.wong@outlook.com>
2022-06-20 19:37:08 -0400

    Adding int2real function and new save functions for split

--- src/backend.jl
+++ src/backend.jl
@@ -23,6 +23,28 @@ function ncoffsetscale(data::AbstractArray{<:Real})
 
 end
 
+function int2real!(
+    oarray :: AbstractArray{FT},
+    iarray :: AbstractArray{Int16};
+    scale  :: Real,
+    offset :: Real,
+    fvalue :: Int16,
+    mvalue :: Int16
+) where FT <: Real
+
+    for ii = 1 : length(iarray)
+
+        if (iarray[ii] == fvalue) || (iarray[ii] == mvalue)
+              oarray[ii] = FT(NaN)
+        else; oarray[ii] = iarray[ii] * scale + offset
+        end
+
+    end
+
+    return
+
+end
+
 function real2int16!(
     oarray :: AbstractArray{Int16},
     iarray :: AbstractArray{<:Real},
--- src/downloads/split.jl
+++ src/downloads/split.jl
@@ -25,9 +25,101 @@ function split(
 
         p = pvec[ip]
         evarii = PressureVariable(evar.varID,hPa=p)
-        save(dataflt,dt,e5ds,evarii,ereg,lsd)
+        save(dataflt,dt,e5ds,evarii,ereg,lsd,sc,of)
     end
 
     close(ds)
+
+end
+
+function save(
+    data :: AbstractArray{<:Real,3},
+    dt   :: Date,
+    e5ds :: ERA5Hourly,
+    evar :: ERA5Variable,
+    ereg :: ERA5Region,
+    lsd  :: LandSea,
+    scale  :: Real,
+    offset :: Real
+)
+
+    @info "$(modulelog()) - Saving raw $(e5ds.lname) $(evar.vname) data in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) ..."
+
+    ds,fnc = save_createds(e5ds,evar,ereg,dt)
+
+    nhr = 24 * daysinmonth(dt)
+
+    ds.dim["longitude"] = length(lsd.lon)
+    ds.dim["latitude"]  = length(lsd.lat)
+    ds.dim["time"] = nhr
+
+    nclon,nclat = save_definelonlat!(ds)
+
+    nctime = defVar(ds,"time",Int32,("time",),attrib = Dict(
+        "units"     => "hours since $(dt) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian",
+    ))
+
+    ncvar = save_definevar!(ds,evar,scale,offset)
+
+    nclon[:]  = lsd.lon
+    nclat[:]  = lsd.lat
+    nctime[:] = collect(1:nhr) .- 1
+
+    if iszero(sum(isnan.(data)))
+          ncvar[:] = data
+    else; ncvar.var[:] = real2int16(data,scale,offset)
+    end
+
+    close(ds)
+
+    @info "$(modulelog()) - Raw $(uppercase(e5ds.lname)) $(evar.vname) in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) $(Dates.monthname(dt)) has been saved into $(fnc)."
+
+end
+
+function save(
+    data :: AbstractArray{<:Real,3},
+    dt   :: Date,
+    e5ds :: ERA5Monthly,
+    evar :: ERA5Variable,
+    ereg :: ERA5Region,
+    lsd  :: LandSea,
+    scale  :: Real,
+    offset :: Real
+)
+
+    @info "$(modulelog()) - Saving raw $(e5ds.lname) $(evar.vname) data in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) ..."
+
+    ds,fnc = save_createds(e5ds,evar,ereg,dt)
+
+    nt = 12; if e5ds.hours; nt = nt * 24 end
+
+    ds.dim["longitude"] = length(lsd.lon)
+    ds.dim["latitude"]  = length(lsd.lat)
+    ds.dim["time"] = nt
+
+    nclon,nclat = save_definelonlat!(ds)
+
+    nctime = defVar(ds,"time",Int32,("time",),attrib = Dict(
+        "units"     => "hours since $(dt) 00:00:00.0",
+        "long_name" => "time",
+        "calendar"  => "gregorian",
+    ))
+
+    ncvar = save_definevar!(ds,evar,scale,offset)
+
+    nclon[:]  = lsd.lon
+    nclat[:]  = lsd.lat
+    nctime[:] = save_definetimes(e5ds,dt)
+
+    if iszero(sum(isnan.(data)))
+          ncvar[:] = data
+    else; ncvar.var[:] = real2int16(data,scale,offset)
+    end
+
+    close(ds)
+
+    @info "$(modulelog()) - Raw $(uppercase(e5ds.lname)) $(evar.vname) in $(ereg.geo.name) (Horizontal Resolution: $(ereg.gres)) for $(year(dt)) has been saved into $(fnc)."
 
 end
