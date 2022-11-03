f8d79e564881b1716acc359e7c158ef108366579
Nathanael Wong <natgeo.wong@outlook.com>
2022-11-03 16:56:21 -0400

    Adding show for ERA5Daily

--- src/dataset/show.jl
+++ src/dataset/show.jl
@@ -4,9 +4,22 @@ function show(io::IO, emod::ERA5Hourly)
 		"The ERA5Hourly Module has the following properties:\n",
 		"    Dataset ID     (e5dID) : ", emod.e5dID, '\n',
 		"    Date Begin     (start) : ", emod.start, '\n',
-		"    Date End        (stop) : ", emod.stop, '\n',
+		"    Date End        (stop) : ", emod.stop,  '\n',
 		"    Back Extension (dtext) : ", emod.dtext, '\n',
-		"    Data Directory (path) : ", emod.path, '\n',
+		"    Data Directory  (path) : ", emod.path,  '\n',
+		"    Mask Directory (emask) : ", emod.emask, '\n',
+	)
+end
+
+function show(io::IO, emod::ERA5Daily)
+    print(
+		io,
+		"The ERA5Daily Module has the following properties:\n",
+		"    Dataset ID     (e5dID) : ", emod.e5dID, '\n',
+		"    Date Begin     (start) : ", emod.start, '\n',
+		"    Date End        (stop) : ", emod.stop,  '\n',
+		"    Back Extension (dtext) : ", emod.dtext, '\n',
+		"    Data Directory  (path) : ", emod.path,  '\n',
 		"    Mask Directory (emask) : ", emod.emask, '\n',
 	)
 end
