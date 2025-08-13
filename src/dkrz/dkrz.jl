b51494b046b6782e3ba01be0cfd7e4a0323a2088
Nathanael Wong <natgeo.wong@outlook.com>
2025-08-13 07:19:54 -0400

    Debug the dkrz function

--- src/dkrz/dkrz.jl
+++ src/dkrz/dkrz.jl
@@ -41,7 +41,7 @@ function dkrz(
         fgrb = "E5$(level)00_$(tres)_2001-01-01_$(@sprintf("%03d",evar.dkrz)).grb"
     end
 
-    return GRIBDataset(path,fgrb)
+    return GRIBDataset(joinpath(path,fgrb))
 
 end
 
