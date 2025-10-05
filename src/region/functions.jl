846fe6e09a9e3467c04a53736e480956c9bcccb6
Nathanael Wong <natgeo.wong@outlook.com>
2025-10-04 20:34:04 -0400

    Adding new RegionGrid method for ERA5Regions on an Unstructured Grid

--- src/region/functions.jl
+++ src/region/functions.jl
@@ -118,3 +118,28 @@ function RegionGrid(
     return RegionGrid(egeo.geo,lon,lat)
 
 end
+
+"""
+    RegionGrid(
+        e5geo :: ERA5Region{ST,FT},
+        lon   :: Vector{<:Real},
+        lat   :: Vector{<:Real}
+    ) -> GeoRegion.RegionGrid
+
+Creates a RegionGrid containing information and mask information required to extract regional data for the ERA5Region from the raw data.
+
+Arguments
+=========
+
+- `e5geo` : A ERA5Region struct type
+- `lon`   : A vector containing the longitude points
+- `lat`   : A vector containing the latitude points
+"""
+function RegionGrid(
+    egeo :: ERA5Region,
+    pnts :: Array{Point2{FT}};
+) where FT <: Real
+
+    return RegionGrid(egeo.geo,pnts)
+
+end
